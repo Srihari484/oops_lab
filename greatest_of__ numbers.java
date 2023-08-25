98649136f1fb77b8a97661018dc1fbc404203957
Srihari484 <121538289+Srihari484@users.noreply.github.com>
2023-08-25 11:13:52 +0530

    Create greatest_of__ numbers.java

---
+++ greatest_of__ numbers.java
@@ -0,0 +1,34 @@
+import java util*.;
+
+class main{
+  public static void main(string[] args)
+  {
+    int n1,n2;
+    Scanner a = new Scanner(System.in);
+    system.out.println("Enter the first number");
+    Scanner b = new Scanner(System.in);
+    system.out.println("Enter the second number");
+
+    n1=a.nextInt();
+    n2=b.nextInt();
+
+    if(n1>n2)
+    {
+      system.out.println("the largest number is :",n1);
+    }
+    else
+    {
+      system.out.println("The largest number is :",n2);
+    }
+    
+    
+    
+
+    
+
+    
+
+    
+
+
+
